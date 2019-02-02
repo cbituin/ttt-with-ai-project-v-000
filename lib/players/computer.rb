d492193967026072b7946fc594dd417bb1836ad6
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-02-02 23:34:34 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -21,14 +21,13 @@ class Players
       # randomMove = r.rand(1...10) 
       # board.valid_move?(randomMove) ? randomMove.to_s : move(board)
 #########     Initial "dumb" logic      ######### 
-    binding.pry
-      
-    #   WIN_COMBINATIONS.any? do |combo| #iterate over WIN_COMBINATIONS
-    # position_1 = board.cells[combo[0]]
-    # position_2 = board.cells[combo[1]]
-    # position_3 = board.cells[combo[2]]
-    # return combo if position_1 == position_2 && position_2 == position_3 && position_1 != " "
-    # end
+
+    WIN_COMBINATIONS.any? do |combo| #iterate over WIN_COMBINATIONS
+    position_1 = board.cells[combo[0]]
+    position_2 = board.cells[combo[1]]
+    position_3 = board.cells[combo[2]]
+    return combo if position_1 == position_2 && position_2 == position_3 && position_1 != " "
+    end
 
 
     end
