77c74640fdf1619abd109d8beae7393dfc94e9be
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-02-02 23:27:27 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,9 +5,15 @@ class Players
   class Computer < Player
     
     def move(board)
-      r = Random.new
-      randomMove = r.rand(1...10) 
-      board.valid_move?(randomMove) ? randomMove.to_s : move(board)
+#########     Initial "dumb" logic      ######### 
+      # r = Random.new
+      # randomMove = r.rand(1...10) 
+      # board.valid_move?(randomMove) ? randomMove.to_s : move(board)
+#########     Initial "dumb" logic      ######### 
+    binding.pry
+      
+
+
     end
     
   end
